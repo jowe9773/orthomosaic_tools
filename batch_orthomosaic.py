074bd131f74ebd3e4d70d6748d0b667893343e0c
Josie Welsh <jowe9773@colorado.edu>
2026-01-27 07:57:05 -0700

    made a batch processing script

---
+++ batch_orthomosaic.py
@@ -0,0 +1,74 @@
+# batch_orthomosaic.py
+
+#import neccesary packages and modules
+from pathlib import Path
+import pandas as pd
+from functions import File_Functions, Video_Functions, Audio_Functions
+from orthomosaic_videos_manual_timing import orthomosaic_video
+
+ff = File_Functions()
+
+#get all video files
+main_directory = "G:/video_data"
+
+# get gcps files
+all_gcps_directory = "C:/Users/jwelsh/Image Annotation/annotated_gcps"
+
+#choose an output location for the final video
+out_vid_dir = "F:/Videos"
+
+#get offsets from offsets_csv
+offsets_csv_fn = "F:/Videos/time_offsets_v1.csv"
+
+offsets_df = pd.read_csv(offsets_csv_fn)
+exps = offsets_df["exp_name"].tolist()
+print("exps", exps)
+
+
+for i, exp in enumerate(exps):
+    exp_name = exp + "_goprodata"
+    print("Exp_name", exp_name)
+    exp_date = exp.split("_")[0]
+    #find the video directory containing the experiment name
+    root = Path(main_directory)
+    matches = [p for p in root.rglob("*") if p.is_dir() and exp_name in p.name]
+    video_dir = matches[0]
+
+    #find the gcps directory containing the experiment name
+    root = Path(all_gcps_directory)
+    matches = [p for p in root.rglob("*") if p.is_dir() and exp_date in p.name]
+    gcps_directory = matches[0]
+    print(gcps_directory)
+
+    #make a list of GCPS file names for the experiment name
+    gcps_filenames = ff.get_gcps_files(gcps_directory)
+
+    #find the time offsets from the csv of times
+    offsets_df = pd.read_csv(offsets_csv_fn)
+    row = offsets_df.loc[offsets_df["exp_name"] == exp, ["Start Time", "Length", "Offset 1", "Offset 2", "Offset 3"]]
+    times_list = row.iloc[0].tolist()
+    print(times_list)
+
+    start_time = times_list[0]
+    length = times_list[1]
+    offsets_list = [times_list[2], times_list[3], times_list[4]]
+
+    out_name = exp_name + "_full.mp4"
+
+    #set video params
+    compression = 2.5
+    speed = 1
+
+    #set up orthomosaic_video 
+    ortho = orthomosaic_video(gcps_filenames= gcps_filenames,
+                            main_dir= video_dir, 
+                            out_location= out_vid_dir, 
+                            out_name= out_name, 
+                            offsets_list= offsets_list, 
+                            compression = compression, 
+                            speed= speed, 
+                            start_time= start_time, 
+                            length = length
+                            )
+
+    ortho.run_orthomosaic()
--- orthomosaic_videos_manual_timing.py
+++ orthomosaic_videos_manual_timing.py
@@ -2,103 +2,171 @@
 
 "import packages and modules"
 from pathlib import Path
+import pandas as pd
 import time
 import cv2
 from functions import File_Functions, Video_Functions, Audio_Functions
 
 
 #define a orthomosaic video function that will process a single experiment
+class orthomosaic_video:
 
+    def __init__(self, gcps_filenames, main_dir, out_location, out_name, offsets_list, compression = 2.5, speed = 1, start_time = 0, length = 60, ):
+        # instantiate classes
+        self.ff = File_Functions()
+        self.af = Audio_Functions()
+        self.vf = Video_Functions()
 
+        # define files/directories
+        self.gcps_filenames = gcps_filenames
+        self.main_dir = main_dir
+        self.out_vid_dir = out_location
+
+        # define important variables
+        self.out_name = out_name
+        self.offsets_list = offsets_list
+        self.compression = compression
+        self.speed = speed
+        self.start_time = start_time
+        self.length = length
+
+        print("orthomosaic_video initialized")
+
+    def _load_targets(self):
+        self.targets = []
+        for i, gcps in enumerate(self.gcps_filenames):
+            gcps = self.ff.import_gcps(gcps)
+            self.targets.append(gcps)
+        
+
+    def _set_offsets(self):
+        # Set offsets
+        off_1 = self.offsets_list[0]                  #if video from downstream cam is late, add time
+        off_2 = self.offsets_list[1]                  #if video from downstream cam is early, subtract time
+        off_3 = self.offsets_list[2]                  #value is in milliseconds
+
+        #Generate time offsets using the first video from each camera
+        self.time_offsets = [0, 0+off_1, 0+off_1+off_2, 0+off_1+off_2+off_3]
+        print("Time offsets for video streams:")
+        print(self.time_offsets)
+
+    
+    def _run_homography(self):
+        print(self.main_dir)
+        #make list of videos in each camera (will use these videos to find homography)
+        self.camera_files = self.ff.get_sorted_video_filenames(self.main_dir)
+        print(self.camera_files)
+
+        #make a list of the first video file for each camera
+        first_vids = [camera[0] for camera in self.camera_files if camera]
+        print(first_vids)
+
+        #define the range of x values that will be used in calculating the error (from 0 to the width of each frame, which in the case of our flume is 2438mm)
+        self.x_range = (0, 2348)
+
+        #Generate homography matricies
+        self.homo_mats = []
+        self.reprojection_errors = []
+        for i, vid in enumerate(first_vids):
+            homography = self.vf.find_homography(i+1, self.targets[i])
+            error = self.vf.calculate_reprojection_error(self.targets[i], homography, x_range = self.x_range, cam = i)
+
+            self.homo_mats.append(homography)
+            self.reprojection_errors.append(error)
+
+    def _set_captures_list(self):
+        #open captures for each video
+        self.captures_lists = []
+        for i, vid_list in enumerate(self.camera_files):
+            captures = []
+            for j, file in enumerate(vid_list):   
+                cap = cv2.VideoCapture(file)
+                captures.append(cap)
+            self.captures_lists.append(captures)
+
+        
+
+    def run_orthomosaic(self):
+        # load targets
+        self._load_targets()
+
+        # Start measuring time
+        start_time = time.time()
+
+        # load time_offsets
+        self._set_offsets()
+
+        self._run_homography()
+
+        self._set_captures_list()
+
+        # Start measuring time
+        start_vid_time = time.time()
+
+        print(self.captures_lists)
+
+        self.vf.orthomosaicing(self.captures_lists, self.time_offsets, self.homo_mats, self.out_vid_dir, self.out_name, self.speed, self.start_time, self.length, self.compression)
+
+        # End measuring time
+        end_time = time.time()
+
+        # Calculate and print the elapsed time
+        elapsed_time = end_time - start_time
+        video_time = end_time - start_vid_time
+
+        print(f"Total time taken: {elapsed_time:.2f} seconds")
+        print(f"Time taken to process frames: {video_time:.2f} seconds")
+        print("Reproection errors: ", self.reprojection_errors)
+        print("Time offsets: ", self.time_offsets)
 
 if __name__ == "__main__":
-
-    #instantiate classes
     ff = File_Functions()
-    af = Audio_Functions()
-    vf = Video_Functions()
-
-    #define important variables
-    COMPRESSION = 2.5
-    SPEED = 1
-    START_TIME = 0
-    LENGTH = 400
-    OUT_NAME = "20240708_exp1_long.mp4"
-
-
-    #define the range of x values that will be used in calculating the error (from 0 to the width of each frame, which in the case of our flume is 2438mm)
-    x_range = (0, 2348)
-
 
     #get all video files
-    main_directory = ff.load_dn("Select a directory with all videos from all of the cameras")
-    camera_files = ff.get_sorted_video_filenames(main_directory)
-    print(camera_files)
+    main_directory = "G:/video_data"
 
-    first_vids = [camera[0] for camera in camera_files if camera]
-    
-    print(first_vids)
-
-    #get gcps files
-    gcps_directory = ff.load_dn("select directory containing gcps files.")
-    gcps_filenames = ff.get_gcps_files(gcps_directory)
-    print(gcps_filenames)
-
-    targets = []
-    for i, gcps in enumerate(gcps_filenames):
-        gcps = ff.import_gcps(gcps)
-        targets.append(gcps)
+    # get gcps files
+    all_gcps_directory = "C:/Users/jwelsh/Image Annotation/annotated_gcps"
 
     #choose an output location for the final video
-    out_vid_dn = ff.load_dn("Select output location for the final video")
+    out_vid_dir = "C:/Users/jwelsh/Image Annotation"
+    #out_vid_dir = ff.load_dn("Select output location for the final video")
 
-    # Start measuring time
-    start_time = time.time()
+    #get offsets from offsets_csv
+    offsets_csv_fn = "C:/Users/jwelsh/Image Annotation/time_offsets.csv"
 
-    off_1 = 6600                  #if video from downstream cam is late, add time
-    off_2 = 7500                  #if video from downstream cam is early, subtract time
-    off_3 = 8500                  #value is in milliseconds
+    #choose an experiment (by name)
+    exp_name = "20240731_exp1"
+    exp_date = exp_name.split("_")[0]
 
-    #Generate time offsets using the first video from each camera
-    time_offsets = [0, 0+off_1, 0+off_1+off_2, 0+off_1+off_2+off_3]
-    print("Time offsets for video streams:")
-    print(time_offsets)
+    #find the video directory containing the experiment name
+    root = Path(main_directory)
+    matches = [p for p in root.rglob("*") if p.is_dir() and exp_name in p.name]
+    video_dir = matches[0]
+    
+    #find the gcps directory containing the experiment name
+    root = Path(all_gcps_directory)
+    matches = [p for p in root.rglob("*") if p.is_dir() and exp_date in p.name]
+    gcps_directory = matches[0]
 
-    # Start measuring time
-    start_vid_time = time.time()
+    #make a list of GCPS file names for the experiment name
+    gcps_filenames = ff.get_gcps_files(gcps_directory)
 
-    #Generate homography matricies
-    homo_mats = []
-    reprojection_errors = []
-    for i, vid in enumerate(first_vids):
-        homography = vf.find_homography(i+1, targets[i])
-        homo_mats.append(homography)
-        error = vf.calculate_reprojection_error(targets[i], homography, x_range = x_range, cam = i)
-        reprojection_errors.append(error)
+    #find the time offsets from the csv of times
+    offsets_df = pd.read_csv(offsets_csv_fn)
+    row = offsets_df.loc[offsets_df["exp_name"] == exp_name, ["Offset 1", "Offset 2", "Offset 3"]]
+    offsets_list = row.iloc[0].tolist()
+    print(offsets_list)
+
+    offsets_list = [7504+100-50-25, 5494+100-25+25-10, 6040-250+50] #if its late add time
+
+    out_name = exp_name + "_test.mp4"
+
+    #initialize orthomosaic class
+    ortho = orthomosaic_video(gcps_filenames, video_dir, out_vid_dir, out_name, offsets_list, compression= 2.5, speed = 1, start_time = 400 , length = 30)
+
+    ortho.run_orthomosaic()
+
+    print(offsets_list)
 
     
-    #open captures for each video
-    captures_lists = []
-    for i, vid_list in enumerate(camera_files):
-        captures = []
-        for j, file in enumerate(vid_list):   
-            cap = cv2.VideoCapture(file)
-            captures.append(cap)
-        captures_lists.append(captures)
-
-    print(captures_lists)
-
-
-    vf.orthomosaicing(captures_lists, time_offsets, homo_mats, out_vid_dn, OUT_NAME, SPEED, START_TIME, LENGTH, COMPRESSION)
-
-    # End measuring time
-    end_time = time.time()
-
-    # Calculate and print the elapsed time
-    elapsed_time = end_time - start_time
-    video_time = end_time - start_vid_time
-
-    print(f"Total time taken: {elapsed_time:.2f} seconds")
-    print(f"Time taken to process frames: {video_time:.2f} seconds")
-    print("Reproection errors: ", reprojection_errors)
-    print("Time offsets: ", time_offsets)
